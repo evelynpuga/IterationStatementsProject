258fd0bae68fe71958cd36a0e3b47b055ef90c8f
Evelyn Puga <pugaevelyn0@gmail.com>
2025-10-06 20:36:43 -0700

    Completed More Loops Practice

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -17,35 +17,31 @@ namespace IterationStatements
 
             //TODO - Create a variable of type int and name it "num"
             //TODO - Initialize the variable with a value of 0
-
+            var num = 0;
 
 
             //TODO - Create a do-while loop using the existing brackets as a template.
-            // The existing brackets are placeholders for your loop code.
-            // Inside this loop:
+
+            do
+            {
+            
             // a) Increment "num" by 1
+            num++;
             // b) Then add "num" to the collection "numbers"
             //    Hint: Use "numbers.Add(num);" to add the current value of "num" to the list.
-
-            {
-                // Your code goes here
-                numbers.Add(/*num*/); //uncomment out `num` to add it to the collection "numbers"
-            } 
-            //TODO - Continue the loop while "num" is less than 100
+                numbers.Add(num);
+            } while (num < 100);
             
 
             //TODO - Create a while loop using the existing brackets as a template.
-            //TODO - Continue the loop while "num" is less than 200
-            // The existing brackets are placeholders for your loop code.
-            // Inside this loop:
-            // a) Increment "num" by 1
-            // b) Then add "num" to the collection "numbers"
-            //    Hint: You can copy how this was done in the do-while loop
-
+            while (num < 200)
             {
-                // Your code goes here
+                // a) Increment "num" by 1
+                num++;
+                // b) Then add "num" to the collection "numbers"
+                //    Hint: You can copy how this was done in the do-while loop
+                numbers.Add(num);
             }
-            
 
 
             // This is to show the user that the numbers will start increasing on the console
@@ -53,10 +49,13 @@ namespace IterationStatements
 
 
             //TODO - Create a foreach loop using the existing brackets as a template.
-            // The existing brackets are placeholders for your loop code.
+            //The existing brackets are placeholders for your loop code.
             // Inside this loop, print each number in "numbers".
             {
-                // Your code goes here
+                foreach (var number in numbers)
+                {
+                    Console.WriteLine(number);
+                }
             }
              
 
@@ -74,7 +73,8 @@ namespace IterationStatements
             //    - Less than or equal to the number of items in "numbers" (use "numbers.Count")
             //    - Greater than or equal to 0
             // c) Decrement "i" by 1 in each iteration
-
+            
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
             {
                 //TODO - Inside the loop, place numbers[i] inside of the Console.WriteLine() method
                 Console.WriteLine(numbers[i]); // Example placement of numbers[i] inside Console.WriteLine
